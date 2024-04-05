d7b9c749c2d3965c609951aa33ab33af39301cf9
Hoangnhat4869 <104480181+Hoangnhat4869@users.noreply.github.com>
2024-04-05 09:58:19 +0700

    update column name

--- src/main.py
+++ src/main.py
@@ -13,8 +13,12 @@ print("Attributes in B: ", B.columns)
 
 
 ## Create new data contains some attributes
-newA = A[['Song_Name', 'Artist_Name', 'Genre', 'Released']]
-newB = B[['track_name', 'artist_name', 'genre', 'release_date']]
+newA = A[['id', 'Song_Name', 'Artist_Name', 'Genre', 'Released']]
+newB = B[['id', 'track_name', 'artist_name', 'genre', 'release_date']]
+
+## Rename columns
+newA.columns = ['id', 'Song_Name', 'Artist_Name', 'Genre', 'Released']
+newB.columns = ['id', 'Song_Name', 'Artist_Name', 'Genre', 'Released']
 
 
 # EVALUATE FILE SIZE
@@ -26,6 +30,10 @@ print('Length matches: ', len(newA) * len(newB))
 print(newA.head(5))
 print(newB.head(5))
 
+# Set key attribute for new data
+em.set_key(newA, 'id')
+em.set_key(newB, 'id')
+
 # DOWNSAMPLE FILES
 A1, B1 = em.down_sample(newA,newB,500,1,show_progress=True)
 print(A1.head(5))
@@ -34,8 +42,14 @@ print("File length after down sampling: ")
 print(len(A1))
 print(len(B1))
 
+em.get_key(A1)
+em.get_key(B1)
+
 
 ## export the downsampled file
-os.makedirs('../newdata', exist_ok=True)
-A1.to_csv('../newdata/tableA_downsampled.csv',index=False)
-B1.to_csv('../newdata/tableB_downsampled.csv',index=False)
+# os.makedirs('../newdata', exist_ok=True)
+# A1.to_csv('../newdata/tableA_downsampled.csv',index=False)
+# B1.to_csv('../newdata/tableB_downsampled.csv',index=False)
+
+
+# PROFILE DATA
