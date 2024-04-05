10d8a21da18f87e5c2d12431ebdfdf1e83eda5da
Khoa Tran <khoa.tran1805@hcmut.edu.vn>
2024-04-05 11:20:10 +0700

    blocker

--- src/blocking.py
+++ src/blocking.py
@@ -4,8 +4,19 @@ import py_entitymatching as em
 import pandas as pd
 import os, sys
 
+# READ FILES
 A = em.read_csv_metadata('../newdata/tableA_downsampled.csv',key='id')
 B = em.read_csv_metadata('../newdata/tableB_downsampled.csv',key='id')
 
-print(A.head(10))
-print(B.head(10))
+# OVERLAP BLOCKER IS CHOSEN ==> INCOMPATIBILITY IN GENRE ATTRIBUTE BETWEEN 2 FILES
+# CREATE OVERLAP BLOCK OBJECT
+ob = em.OverlapBlocker()
+ab = em.AttrEquivalenceBlocker()
+
+# Cob = ob.block_tables(A, B, 'Song_Name', 'Song_Name', overlap_size=2, l_output_attrs=['Song_Name','Artist_Name','Genre','Released'], r_output_attrs=['Song_Name','Artist_Name','Genre','Released'] )
+Cab = ab.block_tables(A, B, 'Song_Name', 'Song_Name', l_output_attrs=['Song_Name','Artist_Name','Genre','Released'], r_output_attrs=['Song_Name','Artist_Name','Genre','Released'])
+
+
+# CHOOSE SUITABLE BLOCKER
+# print(Cob.head(10)) ===> THIS ONE IS NOT SUITABLE
+print(Cab.head(10)) 
