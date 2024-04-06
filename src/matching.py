be90e71719ab8fecadc81eb1e5aa143175d89f20
Khoa Tran <khoa.tran1805@hcmut.edu.vn>
2024-04-06 18:36:12 +0700

    working on matching production

--- src/matching.py
+++ src/matching.py
@@ -5,6 +5,7 @@ import pandas as pd
 import os, sys
 
 #READ LABELED DATA
+
 A = em.read_csv_metadata('../newdata/tableA_downsampled.csv',key='id')
 B = em.read_csv_metadata('../newdata/tableB_downsampled.csv',key='id')
 C = em.read_csv_metadata('../sample/candidate.csv',key='_id')
@@ -16,42 +17,105 @@ G = em.read_csv_metadata('../sample/labeled.csv',
                          fk_rtable='rtable_id') #DO THIS MANUALLY
 # print(G.head(len(G)))
 
-# GENERATE FEATURES
-feature_table = em.get_features_for_matching(A,B,validate_inferred_attr_types=False)
-# print(matching_features.head(10))
-
 
 # CONVERT THE LABELED DATA TO FEATURE VECTORS USING THE FEATURE TABLE
 attrs_from_table = ['ltable_Song_Name', 'ltable_Artist_Name', 'ltable_Released',
                     'rtable_Song_Name', 'rtable_Artist_Name', 'rtable_Released']
 
-
-# Convert the I into a set of feature vectors using F
-H = em.extract_feature_vecs(G,
-                            feature_table=feature_table,
-                            attrs_before = attrs_from_table,
-                            attrs_after='label',
-                            show_progress=True)
-# print(H)
+# Split S into I an J
+IJ = em.split_train_test(G, train_proportion=0.7, random_state=0)
+I = IJ['train']
+J = IJ['test']
 
 
-rf = em.RFMatcher()
+# Create a set of ML-matchers
+dt = em.DTMatcher(name='DecisionTree', random_state=0)
+svm = em.SVMMatcher(name='SVM', random_state=0)
+rf = em.RFMatcher(name='RF', random_state=0)
+lg = em.LogRegMatcher(name='LogReg', random_state=0)
+ln = em.LinRegMatcher(name='LinReg')
 
-# Get the attributes to be projected while training
-attrs_to_be_excluded = []
-attrs_to_be_excluded.extend(['_id', 'ltable_id', 'rtable_id', 'label'])
-attrs_to_be_excluded.extend(attrs_from_table)
 
-# Train using feature vectors from the labeled data.
-rf.fit(table=H, exclude_attrs=attrs_to_be_excluded, target_attr='label')
+feature_table = em.get_features_for_matching(A,B,False)
 
-# Convert the cancidate set to feature vectors using the feature table
-L = em.extract_feature_vecs(C, feature_table=feature_table,
-                             attrs_before= attrs_from_table,
-                             show_progress=True, n_jobs=-1)
+H = em.extract_feature_vecs(I,feature_table=feature_table,attrs_after='label',show_progress=True)
 
-# # Predict the matches
-# predictions = rf.predict(table=L, exclude_attrs=attrs_to_be_excluded,                          
-#               append=True, target_attr='predicted', inplace=False)
+# Select the best ML matcher using CV
+result = em.select_matcher([dt, rf, svm, ln, lg], table=H, 
+        exclude_attrs=['_id', 'ltable_id', 'rtable_id', 'label'],
+        k=4,
+        target_attr='label', metric_to_select_matcher='f1', random_state=0)
 
-# print(predictions.head())
+print(result['cv_stats'])
+
+
+# Convert J into a set of feature vectors using feature table
+L = em.extract_feature_vecs(J, feature_table=feature_table,
+                            attrs_after='label', show_progress=False)
+
+
+# Train using feature vectors from I 
+lg.fit(table=H, 
+       exclude_attrs=['_id', 'ltable_id', 'rtable_id', 'label'], 
+       target_attr='label')
+
+# Predict on L 
+predictions = lg.predict(table=L, exclude_attrs=['_id', 'ltable_id', 'rtable_id', 'label'], 
+              append=True, target_attr='predicted', inplace=False)
+
+
+predictions.to_csv('../sample/predict.csv')
+
+# Evaluate the predictions
+eval_result = em.eval_matches(predictions, 'label', 'predicted')
+em.print_eval_summary(eval_result)
+
+
+
+
+################################# PRODUCTION #################################
+
+def integrate():
+    # # MERGE 2 TABLES
+    # table = tableA.merge(tableB, left_on='ltable_id', right_on='rtable_id')
+    # table.columns = ['ltable_id','rtable_id','ltable_Song_Name','ltable_Artist_Name','ltable_Released','rtable_Song_Name','rtable_Artist_Name','rtable_Released']
+
+    tableA = em.read_csv_metadata('../data/tableA.csv',key='id')
+    tableB = em.read_csv_metadata('../data/tableB.csv',key='id')
+
+    ## Create new data contains some attributes
+    tableA = tableA[['id', 'Song_Name', 'Artist_Name','Released']]
+    tableB = tableB[['id', 'track_name', 'artist_name','release_date']]
+    
+    # ## Rename columns 
+    tableA.columns = ['id', 'Song_Name', 'Artist_Name', 'Released']
+    tableB.columns = ['id', 'Song_Name', 'Artist_Name', 'Released']
+
+    ## Drop NA values
+    tableA = tableA.dropna()
+    tableB = tableB.dropna()
+
+    # CONVERT A TABLE COLUMNS TO LOWERCASE IF DATA IS STRNG TYPE
+    tableA['Song_Name'] = tableA['Song_Name'].str.lower()
+    tableB['Artist_Name'] = tableB['Artist_Name'].str.lower()
+
+    # Set key attribute for new data
+    em.set_key(tableA, 'id')
+    em.set_key(tableB, 'id')
+
+    ## sort the data by id
+    tableA = tableA.sort_values(by=['id'])
+    tableB = tableB.sort_values(by=['id'])
+
+
+    ## Convert A1 released time to year
+    tableA['Released'] = pd.to_datetime(tableA['Released']).dt.year.astype(int)
+
+    
+
+    ab = em.AttrEquivalenceBlocker()
+    C = ab.block_tables(tableA, tableB, 'Song_Name', 'Song_Name', l_output_attrs=['Song_Name','Artist_Name','Released'], r_output_attrs=['Song_Name','Artist_Name','Released'])
+    # print(C.head())
+    
+
+integrate()
